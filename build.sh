f0ce66a2126cbe3f37ec3c8fb3b63e2ce01cc697
Razer5624 <105141988+Razer5624@users.noreply.github.com>
2024-03-25 12:47:23 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ cp .config/docker_example.env .config/docker.env
 cp ./docker-compose_example.yml ./docker-compose.yml
 sudo docker compose build
 sudo docker compose run --rm web pnpm run init
+cd ..
+bash run.sh
