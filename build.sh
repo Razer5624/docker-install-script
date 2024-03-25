af2fd062c39d4a1ca9d167510f8a5dd55f7cbcbc
Razer5624 <105141988+Razer5624@users.noreply.github.com>
2024-03-25 12:45:25 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+git clone -b master https://github.com/misskey-dev/misskey.git
+cd misskey
+git checkout master
+cp .config/docker_example.yml .config/default.yml
+cp .config/docker_example.env .config/docker.env
+cp ./docker-compose_example.yml ./docker-compose.yml
+sudo docker compose build
+sudo docker compose run --rm web pnpm run init
